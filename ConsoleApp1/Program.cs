eb75e8ecda83fbd6e0150f932c6ec81b20d615f9
Ben Woodward <ben@benwoodward.me.uk>
2023-07-09 00:16:23 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿var actions = new List<Func<int>>();
+
+var variable = 0;
+while (variable < 5)
+{
+    actions.Add(() => variable * 2);
+    ++ variable;
+}
+
+foreach (var act in actions)
+{
+    Console.WriteLine(act.Invoke());
+}
